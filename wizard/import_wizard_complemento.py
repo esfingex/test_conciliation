99417c951c960fdc4310cbcf34ceac06619b04ef
Iván Masías <ivan.masias.ortiz@gmail.com>
2022-11-27 16:47:25 -0300

    [ADD] validaciónes
    - Al dar click en “Importar” sólo debe aceptar archivos con nombre “master.xlsx”
    - Si alguna columna no trae encabezado debe notificarlo y no importar datos
    - Si alguna celda viene vacía debe notificarlo y no importar datos

--- wizard/import_wizard_complemento.py
+++ wizard/import_wizard_complemento.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 # Part of Odoo. See LICENSE file for full copyright and licensing details.
+from base64 import b64decode
 
 from xlrd import open_workbook
 from odoo import models, api, fields
@@ -24,15 +25,34 @@ class ImportWizardComplemento(models.TransientModel):
         ext = fname[len(fname)-1]
         name = fname[0]
         
-        if ext != 'xlsx' and name != 'complemento':
-            raise ValidationError("Extensión o nombre incorrecto del archivo")
+        if ext != 'xlsx':
+            raise ValidationError("Extensión incorrecto del archivo")
         
+        if name != 'master':
+            raise ValidationError("Nombre de archivo incorrecto")
         
-        workbook = open_workbook(file_contents=self.xlsx_data)
+        workbook = open_workbook(file_contents=b64decode(self.xlsx_data))
         sheet_names = workbook.sheet_names()
         sheet = workbook.sheet_by_name(sheet_names[0])
         
-        num_rows = sheet.nrows
+        num_rows = sheet.nrows - 1
+        num_cols = sheet.ncols - 1
+        
+        #Cabecera archivo master.xlsx
+        head = ['concept','type','account','tax_information','date']
+    
+        #Valida la cabecera del archivo
+        if sheet._cell_values[0] != head:
+            raise ValidationError("Encabezado no existe, favor agregar encabezado")
+        else:
+            data = []
+            for i in range(1, num_cols):
+                if '' not in sheet._cell_values[i]:
+                    value = dict(zip(head, sheet._cell_values[i]))
+                    data.append((0,0,value))
+                else:
+                    raise ValidationError(("Datos vacios en la linea %s") % i)
+            self.env['complemento'].create(data)
         
         
     
