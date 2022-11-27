87ed0fb22cea444c378bfefdf1430c822226ccea
Iván Masías <ivan.masias.ortiz@gmail.com>
2022-11-27 15:35:41 -0300

    [ADD] se agrega validación de archivos excel
    - se agrega validación de archivos master y complemento

--- wizard/import_wizard_master.py
+++ wizard/import_wizard_master.py
@@ -3,6 +3,7 @@
 
 from xlrd import open_workbook
 from odoo import models, api, fields
+from odoo.exceptions import ValidationError
 
 class ImportWizardMaster(models.TransientModel):
     _name = 'import.wizard.master'
@@ -11,8 +12,21 @@ class ImportWizardMaster(models.TransientModel):
     user_id = fields.Many2one('res.users', string='Usuario', default=lambda self: self.env.user)
     init_date = fields.Date('Fecha Actual', default=lambda self: fields.Datetime.now())
     xlsx_data = fields.Binary('Archivo XLSX', required=True)
+    filename = fields.Char()
     
     def load_data(self):
+    
+        if self.xlsx_data:
+            if not self.xlsx_data:
+                raise ValidationError("Archivo no encontrado")
+
+        fname = self.filename.split('.')
+        ext = fname[len(fname)-1]
+        name = fname[0]
+        
+        if ext != 'xlsx' and name != 'master':
+            raise ValidationError("Extensión o nombre incorrecto del archivo")
+        
         
         workbook = open_workbook(file_contents=self.xlsx_data)
         sheet_names = workbook.sheet_names()
